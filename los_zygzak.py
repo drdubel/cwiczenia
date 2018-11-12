9efb5d50b75eaeb519b336e1ea5fca73fb1a76a0
Antoni Suszko <antoni.suszko@gmail.com>
2018-11-12 21:17:37 +0100

    losowy zygzak

---
+++ los_zygzak.py
@@ -0,0 +1,15 @@
+import turtle
+import random
+
+t=turtle.Pen()
+
+def los_zygzak():
+    while True:
+        t.color(random.random(),random.random(),random.random())
+        t.speed(0)
+        t.fd(random.randrange(0,200))
+        t.right(random.randrange(0,1000))
+        if t.distance(0,0)>300:
+            t.goto(0,0)
+
+los_zygzak()
