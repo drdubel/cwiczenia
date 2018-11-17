1f83399ffb6340c0fc3a503a8b5ac2c2732bcaf5
Antoni Suszko <antoni.suszko@gmail.com>
2018-11-17 19:08:15 +0100

    pap_noz_kam_czarna dziura
    ostateczna wersja 5.0.0

---
+++ pap_kamień_noz.py
@@ -0,0 +1,73 @@
+import random
+KA = 'kamień'
+PA = 'papier'
+NO = 'nożyce'
+CZ = 'czarna_dz'
+
+wygrywa = {
+    KA: PA,
+    PA: NO,
+    NO: KA,
+}
+
+WYGRANA = 1
+PRZEGRANA = -1
+REMIS = 0
+
+def sedzia(ah, oh):
+    if ah == oh:
+        return REMIS
+    if ah == CZ:
+        return WYGRANA
+    if oh == CZ:
+        return PRZEGRANA
+    if wygrywa[ah] == oh:
+        return PRZEGRANA
+    else:
+        return WYGRANA
+
+def marceli(z):
+    while True:
+        trut = input("wybierz papier, nożyce, kamień czy czarną dziurę: ")
+        if trut in z:
+            return trut
+        print("źle!")
+
+
+def p_k_n_c(tury, pkt_b, pkt_a):
+    pkt = 0
+    while pkt_b or pkt_a <= tury:
+        z = [PA, KA, NO, CZ]
+        ah = marceli(z)
+        oh = random.choice(z)
+        koniec = sedzia(ah, oh)
+        pkt += koniec
+        print('komputer wybrał:', oh)
+        print('gracz wybrał:', ah)
+        if koniec == REMIS:
+            print('remis')
+            print('gracz ma', pkt_b, 'a komputer', pkt_a) 
+        if koniec == WYGRANA:
+            print('wygrał gracz')
+            pkt_b += 1
+            print('gracz ma', pkt_b, 'a komputer', pkt_a) 
+        if koniec == PRZEGRANA:
+            print('przegrał gracz a wygrał komputer')
+            pkt_a += 1
+            print('gracz ma', pkt_b, 'a komputer', pkt_a)
+        if pkt_b == 3:
+            if tury - pkt_a == 1:
+                print('gracz wygrał', 'jednym punktem')
+            else:
+                print('gracz wygrał', tury - pkt_a, 'punktami')
+            return
+        if pkt_a == 3:
+            if tury - pkt_b == 1:
+                print('komputer wygrał', 'jednym punktem')
+            else:
+                print('komputer wygrał', tury - pkt_b, 'punktami')
+            return
+    
+
+
+p_k_n_c(3, 0, 0)
