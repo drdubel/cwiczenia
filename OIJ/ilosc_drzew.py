f9d7fd7d42196b909fef8f1e00bde171feb685c7
DrDubel <antoni.suszko@gmail.com>
2021-04-25 15:45:18 +0200

    zadanie z programowania dynamicznego z OIJ https://sio2.mimuw.edu.pl/c/zwo20/p/zli/

---
+++ OIJ/ilosc_drzew.py
@@ -0,0 +1,11 @@
+def grafne_dzewa(il_drzew, dzielnik):
+    if 1 == il_drzew:
+        return il_drzew
+    return il_drzew**(il_drzew-2)%dzielnik
+
+def main():
+    il_drzew, dzielnik = [int(i) for i in input().split()]
+    print(grafne_dzewa(il_drzew, dzielnik))
+
+if __name__ == "__main__":
+    main()
