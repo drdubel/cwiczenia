f3d763c03bb799885d740fdb70247aca7d2ad6a3
DrDubel <antoni.suszko@gmail.com>
2021-04-25 15:53:50 +0200

    Zadania ze Zdalnych Warsztatów Olimpijskich z 2020 roku https://sio2.mimuw.edu.pl/c/zwo20/p/

---
+++ OIJ/lasderzw.py
@@ -0,0 +1,10 @@
+def czydrzewo(il_drzew, il_kraw, kraw):
+    if il_drzew-2 > il_kraw:
+        return "niedrzewo", kraw
+    return "drzewo"
+
+def main():
+    il_drzew, il_kraw = list(map(int, input().split()))
+    kraw = {}
+    for i in range(il_kraw):
+        
