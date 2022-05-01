1c4ecfc0661cc8e9709ba98416a37db2a40532a9
DrDubel <antoni.suszko@gmail.com>
2022-05-01 19:49:32 +0200

    Działające acz zbyt wolne rozwiązanie z użyciem numpy

--- Python/OIJ/ciezarowki/ciezarowki.py
+++ Python/OIJ/ciezarowki/ciezarowki.py
@@ -2,66 +2,53 @@ import sys
 import numpy as np
 
 
-def przeladunek(nes_x, nes_v, ciezarowki, poprzedni_czas, cel):
-    if nes_v:
-        eta = abs(cel - nes_x) / nes_v
-    else:
-        eta = 2**31 - 1
-    szybsze = []
-    najlepsza = (eta, -nes_v, nes_x)
-    if len(ciezarowki):
-        for c_x, c_v in ciezarowki:
-            if c_v <= nes_v:
-                continue
-            if nes_x < c_x:
-                c_x -= poprzedni_czas * c_v
-            else:
-                c_x += poprzedni_czas * c_v
-            if nes_x < c_x:
-                if nes_x < cel:
-                    do_spotkania = (c_x - nes_x) / (c_v + nes_v)
-                else:
-                    do_spotkania = (c_x - nes_x) / (c_v - nes_v)
-            elif c_x < nes_x:
-                if cel < nes_x:
-                    do_spotkania = (nes_x - c_x) / (c_v + nes_v)
-                else:
-                    do_spotkania = (nes_x - c_x) / (c_v - nes_v)
-            else:
-                do_spotkania = 0
-            if do_spotkania > eta:
-                continue
-            if (do_spotkania, -c_v) < najlepsza:
-                najlepsza = (do_spotkania, -c_v, c_x)
-            szybsze.append((c_x, c_v))
-    do_spotkania, mnes_v, _nes_x = najlepsza
-    if nes_v:
-        if nes_x < cel:
-            nes_x += do_spotkania * nes_v
-        else:
-            nes_x -= do_spotkania * nes_v
-    return do_spotkania, nes_x, -mnes_v, szybsze
-
-
 def ciezarowki(il_ciezarowek, algogrod, bajtogrod, lista_ciezarowek):
-    nes_x = algogrod
     nes_v = 0
-    czas_etapu = 0
-    czas_calkowity = 0
-    for _ in range(500_000):
-        czas_etapu, nes_x, nes_v, lista_ciezarowek = przeladunek(nes_x, nes_v, lista_ciezarowek, czas_etapu, bajtogrod)
-        czas_calkowity += czas_etapu
-        if not lista_ciezarowek:
-            czas_calkowity += abs(bajtogrod - nes_x) / nes_v
-            break
-    return czas_calkowity
+    eta = 2**31 - 1
+    czas_do_konca = 0
+    for _ in range(il_ciezarowek):
+        wybor = [
+            lista_ciezarowek["x"] / (lista_ciezarowek["v"] + nes_v),
+            -lista_ciezarowek["x"] / (lista_ciezarowek["v"] - nes_v),
+        ]
+        warunki = [lista_ciezarowek["x"] > 0, lista_ciezarowek["x"] < 0]
+        lista_ciezarowek["t"] = np.select(warunki, wybor, 0)
+        czas_do_nesesera, _, nes_v2 = lista_ciezarowek[lista_ciezarowek["t"].argmin()]
+        if eta <= czas_do_nesesera:
+            return czas_do_konca + eta
+        czas_do_konca += czas_do_nesesera
+        lista_ciezarowek["x"] -= (
+            lista_ciezarowek["v"]
+            * czas_do_nesesera
+            * lista_ciezarowek["x"]
+            / abs(lista_ciezarowek["x"])
+            + nes_v * czas_do_nesesera
+        )
+        bajtogrod -= nes_v * czas_do_nesesera
+        eta = bajtogrod / nes_v2
+        warunek = lista_ciezarowek["v"] > nes_v2
+        lista_ciezarowek = np.extract(warunek, lista_ciezarowek)
+        if len(lista_ciezarowek) == 0:
+            return czas_do_konca + eta
+        nes_v = nes_v2
 
 
 def main(data_fd):
     il_ciezarowek, algogrod, bajtogrod = map(int, data_fd.readline().strip().split())
-    dtype = np.dtype([("x", np.int32), ("v", np.int32)])
-    lista_ciezarowek = np.loadtxt(data_fd, dtype, max_rows=il_ciezarowek, delimiter=" ")
-    wynik = ciezarowki(il_ciezarowek, algogrod, bajtogrod, lista_ciezarowek)
+    lista_ciezarowek = np.zeros(
+        il_ciezarowek, dtype=[("t", np.float64), ("x", np.float64), ("v", np.int32)]
+    )
+    if algogrod > bajtogrod:
+        znak = -1
+    else:
+        znak = 1
+    for slot, line in zip(lista_ciezarowek, data_fd):
+        sx, sv = line[:-1].split()
+        slot[1] = (int(sx) - algogrod) * znak
+        slot[2] = int(sv)
+    wynik = ciezarowki(
+        il_ciezarowek, 0, (bajtogrod - algogrod) * znak, lista_ciezarowek
+    )
     return ["%.9f" % wynik]
 
 
