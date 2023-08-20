e2c4849c53268bf7bb72db27d26e7cc1682771e3
drdubel <44920161+drdubel@users.noreply.github.com>
2023-08-20 17:42:13 +0000

    zadanie hello world z offa

---
+++ wwi/off/hell/task8.cpp
@@ -0,0 +1,14 @@
+main() {
+    asm(
+        "movl $11, %%edx \n"
+        "movq $0, %%rcx \n"
+        "movq $1, %%rax \n"
+        "movq $1, %%rdi \n"
+        "movq %0, %%rsi \n"
+        "syscall \n"
+        :
+        : "r" ("Hello world")
+        : "%rax", "%rdi", "%rsi", "%rdx", "%rcx"
+    );
+
+}
