b2353491a64b30f652aab0127ecc6699bfcf5e18
drdubel <antek.suszko@gmail.com>
2024-04-06 15:24:02 +0200

    debug

--- pozostale/python/statki/main.py
+++ pozostale/python/statki/main.py
@@ -1,4 +1,5 @@
 import random
+import time
 
 import numpy as np
 
@@ -19,7 +20,6 @@ class Game:
         self.ship_lengths2 = [2, 3, 3, 4, 5]
 
     def place_ship(self, x, y, length, player, vertical=False):
-
         if player == 1:
             ship_id = length - 2 if length != 3 or not self._ships1[1][1] else 4
 
@@ -54,11 +54,9 @@ class Game:
                 if player == 1:
                     for ship in self._ships2:
                         if (x, y) in ship[1]:
-                            print(ship[1], x, y)
                             ship[1].remove((x, y))
 
                             if not ship[1]:
-                                print(ship[0])
                                 self.ship_lengths2.remove(ship[0])
                                 return "sunk"
 
@@ -75,9 +73,12 @@ class Game:
         player1 = Player(self, 1)
         player2 = Player(self, 2)
 
+        print(self._board1)
         while self.ship_lengths1 and self.ship_lengths2:
             player1.move()
             player2.move()
+
+        print(player1.moves, player2.moves)
 
 
 class Player:
@@ -85,15 +86,41 @@ class Player:
         self.game = game
         self.board = get_new_board()
         self.id = id
+        self.moves = 0
 
         self.place_ships()
 
     def place_ships(self):
-        self.game.place_ship(4, 4, 5, self.id, vertical=True)
-        self.game.place_ship(2, 2, 4, self.id, vertical=True)
-        self.game.place_ship(3, 6, 3, self.id)
-        self.game.place_ship(6, 6, 3, self.id)
-        self.game.place_ship(0, 0, 2, self.id, vertical=True)
+        for ship in self.game._ships1:
+            while True:
+                x = random.randint(0, 9)
+                y = random.randint(0, 9)
+                vertical = random.choice([True, False])
+
+                if (
+                    (vertical and x + ship[0] <= 10)
+                    or (not vertical and y + ship[0] <= 10)
+                ) and (
+                    (vertical and (self.board[x : x + ship[0], y] == -1).all())
+                    or (not vertical and (self.board[x, y : y + ship[0]] == -1).all())
+                ):
+                    self.game.place_ship(x, y, ship[0], self.id, vertical)
+
+                    if vertical:
+                        self.board[
+                            max(0, x - 1) : min(10, x + ship[0] + 1),
+                            max(0, y - 1) : min(10, y + 2),
+                        ] = 0
+
+                    else:
+                        self.board[
+                            max(0, x - 1) : min(10, x + 2),
+                            max(0, y - 1) : min(10, y + ship[0] + 1),
+                        ] = 0
+
+                    break
+
+        self.board = get_new_board()
 
     def count_possible_ships(self):
         possibilities = np.zeros((10, 10), dtype=np.int8)
@@ -116,11 +143,6 @@ class Player:
 
         return possibilities
 
-    def shoot_ship(self):
-        x, y = random.choice(np.fromiter(zip(*np.where(self.board == 2)), dtype=tuple))
-
-        return x, y
-
     def get_best_move(self):
         possibilities = self.count_possible_ships()
         x = possibilities.argmax() // 10
@@ -128,68 +150,78 @@ class Player:
 
         return x, y
 
+    def shoot_ship(self):
+        x, y = random.choice(np.fromiter(zip(*np.where(self.board == 2)), dtype=tuple))
+
+        return x, y
+
     def move(self):
+        self.moves += 1
+
         if (self.board == 2).any():
-            print("Shooting ship")
             x, y = self.shoot_ship()
 
         else:
             x, y = self.get_best_move()
 
-        print(f"Shooting at {x}, {y}")
         result = self.game.shoot(x, y, self.id)
 
         if result == "hit":
-            print("Hit!")
             self.board[x, y] = 1
 
-            if self.board[x - 1, y] == 1 or self.board[x + 1, y] == 1:
+            if (x - 1 >= 0 and self.board[x - 1, y] == 1) or (
+                x + 1 < 10 and self.board[x + 1, y] == 1
+            ):
                 wrong = (self.board[x - 1 : x + 2, y - 1 : y + 2] == 2)[:, ::2]
-                print(wrong)
                 wrong[wrong == 2] = 0
 
-            elif self.board[x, y - 1] == 1 or self.board[x, y + 1] == 1:
+            elif (y - 1 >= 0 and self.board[x, y - 1] == 1) or (
+                y + 1 < 10 and self.board[x, y + 1]
+            ) == 1:
                 wrong = [self.board[x - 1 : x + 2, y - 1 : y + 2] == 2][::2]
-                print(wrong)
                 wrong[wrong == 2] = 0
 
-            if self.board[x - 1, y] == -1:
-                self.board[x - 1, y] = 2
+            if x - 1 < 0 or x + 2 > 10 or y - 1 < 0 or y + 2 > 10:
+                print("nei działa")
 
-            if self.board[x + 1, y] == -1:
-                self.board[x + 1, y] = 2
+            neighbors = self.board[
+                max(x - 1, 0) : min(10, x + 2), max(y - 1, 0) : min(10, y + 2)
+            ]
+            neighbors[neighbors == -1] = 2
 
-            if self.board[x, y - 1] == -1:
-                self.board[x, y - 1] = 2
+            if x - 1 < 0:
+                corners = neighbors[1, ::2]
 
-            if self.board[x, y + 1] == -1:
-                self.board[x, y + 1] = 2
+            if x + 1 > 10:
+                corners = neighbors[0, ::2]
 
-            neighbors = self.board[x - 1 : x + 2, y - 1 : y + 2]
+            if y - 1 < 0:
+                corners = neighbors[::2, 1]
+
+            if y + 1 > 10:
+                corners = neighbors[::2, 0]
+
             corners = neighbors[::2, ::2]
 
-            corners[corners == -1] = 0
+            corners[corners == 2] = 0
 
             print(self.board)
-            print("coś nie tak?")
+
             self.move()
 
         elif result == "sunk":
-            print("Hit and sunk!")
             self.board[x, y] = 1
 
-            neighbors = self.board[x - 1 : x + 2, y - 1 : y + 2]
+            neighbors = self.board[
+                max(x - 1, 0) : min(10, x + 2), max(y - 1, 0) : min(10, y + 2)
+            ]
 
             neighbors[neighbors == -1] = 0
 
-            print(self.board)
             self.move()
 
         elif result == "miss":
-            print("Miss!")
-            print(x, y)
             self.board[x, y] = 0
-            print(self.board)
 
 
 def main():
@@ -199,4 +231,7 @@ def main():
 
 
 if __name__ == "__main__":
+    start = time.time()
     main()
+    end = time.time()
+    print(end - start)
